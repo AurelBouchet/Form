30e45edafecf6f164dc9c11c624e65badb43b5a5
AurelBouchet <aurelien-bouchet@hotmail.fr>
2022-03-07 16:00:46 +0100

    reactiveForm initialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SearchMovieComponent } from './search-movie/search-movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchMovieComponent
   ],
   imports: [
     BrowserModule
