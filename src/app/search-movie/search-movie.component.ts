6618144e34eacc61baf66775e098c4098cb07f05
Aurélien BOUCHET <a.bouchet@groupeonepoint.com>
2022-03-08 12:54:50 +0100

    first proto form

--- src/app/search-movie/search-movie.component.ts
+++ src/app/search-movie/search-movie.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component} from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+// import { FormBuilder } from '@angular/forms';
+// import { FormArray } from '@angular/forms';
 @Component({
   selector: 'app-search-movie',
   templateUrl: './search-movie.component.html',
   styleUrls: ['./search-movie.component.scss']
 })
-export class SearchMovieComponent implements OnInit {
+export class SearchMovieComponent {
+title ="Mon Formulaire";
+loginForm = new FormGroup({
+  user : new FormControl(''),
+  password : new FormControl(''),
+})
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+loginUser(){
+  console.log(this.loginForm)
+}
 }
